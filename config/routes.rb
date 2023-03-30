b84ceb6e71353c48ccc420aca421d3c740036877
ryo nishinohara <nishinohara1023@gmail.com>
2023-03-30 21:17:45 +0200

    add search page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/search', to: 'test#search'
   get '/result', to: 'test#result'
   get '/map', to: 'test#map'
+  get '/test', to: 'test#test'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
