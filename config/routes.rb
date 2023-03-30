375cd40695e2565a08e26867c9e8377d17d6569c
ryo nishinohara <nishinohara1023@gmail.com>
2023-03-30 17:15:27 +0200

    add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'test#top'
+  get '/search', to: 'test#search'
+  get '/result', to: 'test#result'
+  get '/map', to: 'test#map'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
