83466ec138abe71609e5c9ed06296c669a141069
alphasyogi <aritomitaiki@gmail.com>
2023-03-31 09:05:54 +0900

    コンフリクトのためルーティングのみプッシュ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'test#top'
+  get '/search', to: 'test#search'
+  get '/result', to: 'test#result'
+  get '/map', to: 'test#map'
+  get '/test', to: 'test#test'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
