ff40b38c5483e752f0ddbf2306b92df060adbab9
ryo nishinohara <nishinohara1023@gmail.com>
2023-04-03 21:18:04 +0200

    add distance caleculating

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -35,40 +35,54 @@ class SearchController < ApplicationController
       section = 'day'
     end
 
+        # 以下は現在地を取得して緯度経度を渡す予定
+        address = '東京都新宿区高田馬場４丁目７−５'
+        params = URI.encode_www_form({q: address})
+        uri = URI.parse("https://msearch.gsi.go.jp/address-search/AddressSearch?#{params}")
+        response = Net::HTTP.get_response(uri)
+        addressResult = JSON.parse(response.body)
+        @currentLatitude = addressResult[0]["geometry"]["coordinates"][1]
+        @currentLongitude = addressResult[0]["geometry"]["coordinates"][0]
+        paramsLatitude1 = URI.encode_www_form({latitude1: @currentLatitude})
+        paramsLongitude1 = URI.encode_www_form({longitude1: @currentLongitude})
+
     while i < arrayLength do
       hash = Hash.new
       if result["values"][i][2] == classification and result["values"][i][3] == number and section == result['values'][i][5]
-        id = result["values"][i][0]
-        hash[:id] = id.to_i
+        #id = result["values"][i][0]
+        #hash[:id] = id.to_i
         hash[:name] = result["values"][i][1]
         hash[:price] = result["values"][i][4].to_i * @length_h
+
+        #住所から緯度経度を求める
         address = result["values"][i][7]
         params = URI.encode_www_form({q: address})
         uri = URI.parse("https://msearch.gsi.go.jp/address-search/AddressSearch?#{params}")
         response = Net::HTTP.get_response(uri)
         addressResult = JSON.parse(response.body)
-        lantitude = addressResult[0]["geometry"]["coordinates"][1]
-        longitude = addressResult[0]["geometry"]["coordinates"][0]
-        hash[:lantitude] = lantitude
-        hash[:longitude] = longitude
-        @array.push(hash)
+        objectLatitude = addressResult[0]["geometry"]["coordinates"][1]
+        objectLongitude = addressResult[0]["geometry"]["coordinates"][0]
+        hash[:latitude] = objectLatitude
+        hash[:longitude] = objectLongitude
+
+        #距離の測量
+        paramsLatitude2= URI.encode_www_form({latitude2: objectLatitude})
+        paramsLongitude2 = URI.encode_www_form({longitude2: objectLongitude})
+        distanceUri = URI.parse("https://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/bl2st_calc.pl?#{paramsLatitude1}&#{paramsLongitude1}&#{paramsLatitude2}&#{paramsLongitude2}&outputType=json&ellipsoid=bessel")
+        distanceResponse = Net::HTTP.get_response(distanceUri)
+        distanceResult = JSON.parse(distanceResponse.body)
+        distance = distanceResult["OutputData"]["geoLength"].to_i
+
+        #1.2km以内の検索結果を配列に入れる
+        if distance <= 1200
+          hash[:distance] = distance
+          @array.push(hash)
+        end
       end
       i = i + 1
     end
     @priceOrderArray = @array.sort_by { |a| a[:price] }
-    @distanceOrderArray = @array.sort_by { |a| a[:id] }
+    @distanceOrderArray = @array.sort_by { |a| a[:distance] }
     @sortedArrayLength = @array.length
-
-    # 以下は現在地を取得して緯度経度を渡す予定
-    address = '東京都新宿区高田馬場４丁目７−５'
-    @params = URI.encode_www_form({q: address})
-    @uri = URI.parse("https://msearch.gsi.go.jp/address-search/AddressSearch?#{@params}")
-    @response = Net::HTTP.get_response(@uri)
-    @result = JSON.parse(@response.body)
-    @lantitude = @result[0]["geometry"]["coordinates"][1]
-    @longitude = @result[0]["geometry"]["coordinates"][0]
-  end
-
-  def map
   end
 end
